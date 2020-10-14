2f45aa265b5da02e2252bd59ccceb90c4fefd52a
yamauchi-shinji <gavetomefor258@gmail.com>
2020-10-14 20:31:34 +0900

    2回目 ユーザー管理機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
-  validates :password, format: { with: PASSWORD_REGEX }
-  validates :nickname, presence: true
-  JAPANESE_REGEX = /\A[あ-んア-ン一-龥]+\z/i
-  validates :family_name, presence: true, format: { with: JAPANESE_REGEX }
-  validates :first_name, presence: true, format: { with: JAPANESE_REGEX }
-  KATAKANA_REGEX = /\A[ア-ン]+\z/i
-  validates :family_name_reading, presence: true, format: { with: KATAKANA_REGEX }
-  validates :first_name_reading, presence: true, format: { with: KATAKANA_REGEX }
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+    validates :nickname
+    validates :family_name, format: { with: /\A[あ-んア-ン一-龥]+\z/i }
+    validates :first_name, format: { with: /\A[あ-んア-ン一-龥]+\z/i } 
+    validates :family_name_reading, format: { with: /\A[ア-ン]+\z/i }
+    validates :first_name_reading, format: { with: /\A[ア-ン]+\z/i}
+    validates :birthday
+  end
 end
