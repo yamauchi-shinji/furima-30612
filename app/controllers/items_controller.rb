148df07c8422d4a1317b941947e14fe2968c300a
yamauchi-shinji <gavetomefor258@gmail.com>
2020-10-16 19:25:58 +0900

    2回目 商品出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, only: [:new]
+  before_action :authenticate_user!, only: [:new]
   def index
     @items = Item.all
   end
@@ -10,7 +10,6 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    @item.save
     if @item.save
       redirect_to root_path
     else
@@ -28,11 +27,5 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:product, :description, :category_id, :status_id, :shipping_charge_id, :prefecture_id, :day_id, :price, :image).merge(user_id: current_user.id)
-  end
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to user_session_path
-    end
   end
 end
