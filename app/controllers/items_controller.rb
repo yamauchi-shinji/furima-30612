4c12213b944ef333f66479fb0024da7c6fe3fa7c
yamauchi-shinji <gavetomefor258@gmail.com>
2020-10-19 16:03:15 +0900

    1回目 商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_to_index, only: [:edit]
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -22,11 +23,27 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
 
   def item_params
     params.require(:item).permit(:product, :description, :category_id, :status_id, :shipping_charge_id, :prefecture_id, :day_id, :price, :image).merge(user_id: current_user.id)
+  end
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id && !@item.purchase.present? 
+      redirect_to action: :index
+    end
   end
 end
