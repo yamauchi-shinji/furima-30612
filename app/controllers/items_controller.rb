41161cf612dd2833743b50b442f841e002dc81d7
yamauchi-shinji <gavetomefor258@gmail.com>
2020-10-19 18:28:47 +0900

    2回目 商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
-  before_action :move_to_index, only: [:edit]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :move_to_index, only: [:edit, :destroy]
   before_action :item_find_params, only: [:show, :edit, :update, :destroy]
   def index
     @items = Item.all.order("created_at DESC")
