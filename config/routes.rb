e836376153123f06391940ebcffd5f2e273b99be
yamauchi-shinji <gavetomefor258@gmail.com>
2020-10-21 20:58:22 +0900

    3回目 商品購入機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
+  resources :items do
     resources :orders, only: [:index, :create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
